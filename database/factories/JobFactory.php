e615c2a15591cf18d1a92d4b8552e0e8313733ad
Ashiqur Rahman Shohan <68808978+ashiqur-shohan@users.noreply.github.com>
2025-11-03 11:23:07 +0600

    job factory added

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -12,7 +12,7 @@ use App\Models\Job;
 class JobFactory extends Factory
 {
 
-    protected $model = Job::class;
+    
 
     /**
      * Define the model's default state.
@@ -27,7 +27,7 @@ class JobFactory extends Factory
             'description' => $this->faker->paragraphs(2, true),
             'salary' => $this->faker->numberBetween(40000, 120000),
             'tags' => implode(',', $this->faker->words(3)),
-            'job_type' => $this->faker-> randomElement(['Full-Time', 'Part-Time', 'Contract', 'Temporary', 'Internship', 'Volunteer', 'On-Call']),
+            'job_type' => $this->faker-> randomElement(['Full-Time', 'Part-Time', 'Contract']),
             'remote' => $this->faker->boolean(),
             'requirements' => $this->faker->sentences(3, true),
             'benefits' => $this->faker->sentences(2, true),
@@ -36,7 +36,7 @@ class JobFactory extends Factory
             'state' => $this->faker->state(),
             'zipcode' => $this->faker->postcode(),
             'contact_email' => $this->faker->safeEmail(),
-            'contact_phone' => $this->faker->phoneEmail(),
+            'contact_phone' => $this->faker->phoneNumber(),
             'company_name' => $this->faker->company(),
             'company_description' => $this->faker->paragraphs(2,true),
             'company_logo' => $this->faker->imageUrl(100,100, 'business',true, 'logo'),
