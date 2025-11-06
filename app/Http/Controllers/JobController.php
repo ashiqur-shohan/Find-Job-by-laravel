1318ed7b4a2edb5910b680df1ec419950ed51edb
Ashiqur Rahman Shohan <68808978+ashiqur-shohan@users.noreply.github.com>
2025-11-06 17:23:57 +0600

    policy added

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Job;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Models\Job;
 class JobController extends Controller
 {
+    use AuthorizesRequests;
+
     // @desc Show all Job listings
     // @route GET / jobs
     public function index()
@@ -50,7 +53,7 @@ class JobController extends Controller
         ]);
 
         //hardcoded user id 
-        $validateData['user_id'] = 1;
+        $validateData['user_id'] = Auth::user()->id;
 
         //check for image
         if($request->hasFile('company_logo')){
@@ -78,6 +81,9 @@ class JobController extends Controller
     // @route GET /jobs/{$id}/edit
     public function edit(Job $job)
     {
+        // Check if user is authorized 
+        $this->authorize('update',$job);
+        
         return view('jobs.edit')->with('job',$job);
     }
 
@@ -85,6 +91,9 @@ class JobController extends Controller
     // @route PUT /jobs/{$id}
     public function update(Request $request, Job $job)
     {
+        // Check if user is authorized 
+        $this->authorize('update',$job);
+
         $validateData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
@@ -129,6 +138,9 @@ class JobController extends Controller
     // @route DELETE /jobs/{$id}
     public function destroy(Job $job)
     {
+        // Check if user is authorized 
+        $this->authorize('delete',$job);
+
         // If logo, then delete it
         if($job->company_logo){
             Storage::disk('public')->delete($job->company_logo);
