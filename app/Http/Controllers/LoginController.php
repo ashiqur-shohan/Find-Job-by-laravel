b4e3116ce2ad31c3193d453c33ab9ff2816f00a5
Ashiqur Rahman Shohan <68808978+ashiqur-shohan@users.noreply.github.com>
2025-11-06 12:55:41 +0600

    login and register functionality added

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     // @desc Show login form
     // @route GET / login
-    public function login(Request $request){
+    public function login( ){
         return view("auth.login");
+    }
+
+    // @desc Authenticate user 
+    // @route POST /login
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'email'=> 'required|string|email|max:100',
+            'password'=> 'required|string',
+        ]);
+
+        // Attempt to authenticate user
+        if(Auth::attempt($credentials)){
+            //regenerate the session to prevent fixation attacks
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'))->with('success','You are now logged in!');
+        }
+
+        //If auth fails, redirect back with error
+        return back()->withErrors([
+            'email'=> 'The provided credentials do not match out records'
+        ])->onlyInput('email');
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     // @desc Show register form
     // @route GET / register
-    public function register(Request $request){
+    public function register(){
         return view("auth.register");
+    }
+
+    // @desc Store user in database
+    // @route POST / register
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            "name" => 'required | string | max:100',
+            'email'=> 'required|string|email|max:100|unique:users',
+            'password'=> 'required|string|min:8|confirmed',
+        ]);
+
+        //hash password
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        //create user
+        $user = User::create($validatedData);
+
+        return redirect()->route('login')->with('success','You are registered, can login');
     }
 }
